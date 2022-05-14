34ac6649d99b64c2c3ca73bb6ac0f0c38847be36
maxim <maxme.85@gmail.com>
2022-05-14 19:39:36 +0400

    master

--- app/js/common.js
+++ app/js/common.js
@@ -44,11 +44,30 @@ var app = new Vue({
 			max: 50,
 			current: 1,
 			step: 1,
-			delta: 2.4,
+			delta: 30350,
+			sum: 0
+		},{
+			title:'Субаренда аппартаментов',
+			min: 1,
+			max: 50,
+			current: 1,
+			step: 1,
+			delta: 30350,
+			sum: 0
+		},{
+			title:'Субаренда квартир долгосрочно',
+			min: 1,
+			max: 50,
+			current: 1,
+			step: 1,
+			delta: 30350,
 			sum: 0
 		}]
 	},
 	methods: {
+		set_calc_current(item, current){
+			item.current = current;
+		},
 		step_changer(item){
 			console.log(item);
 			return Math.round(item.current*item.delta);
